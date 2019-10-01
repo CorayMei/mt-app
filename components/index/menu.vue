305575cba617acf0189a1b75605bbbb130052f25
Coray <820563721@qq.com>
2019-10-01 23:46:43 +0800

    project

---
+++ components/index/menu.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="m-menu">
+    <dl class="nav" @mouseleave="mouseleave">
+      <dt>全部分类</dt>
+      <dd v-for="(item,idx) in menu" :key="idx" @mouseenter="enter">
+        <i :class="item.type"/>{{item.name}}<span class="arrow"/>
+      </dd>
+    </dl>
+    <div class="detail" v-if="kind" @mouseenter="sover" @mouseleave="sout">
+      <template v-for="(item,idx) in curldetail.child">
+        <h4 :key="idx">{{item.title}}</h4>
+        <span v-for="v in item.child" :key="v">{{v}}</span>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      kind: '',
+      menu: [{
+        type: 'food',
+        name: '美食',
+        child: [{
+          title: '美食',
+          child: ['代金券', '甜点饮品', '火锅', '自助餐', '小吃']
+        }]
+      }, {
+        type: 'takeout',
+        name: '外卖',
+        child: [{
+          title: '外卖',
+          child: ['美团外卖']
+        }]
+      }, {
+        type: 'hotel',
+        name: '酒店',
+        child: [{
+          title: '酒店星级',
+          child: ['五星级', '舒适性', '性价比', '好评优先', '离我最近'],
+          // title2:'热门酒店',
+          // child2:['复式楼','别墅']
+        }]
+      }]
+    }
+  },
+  computed: {
+    curldetail: function () {
+      return this.menu.filter((item) => item.type === this.kind,)[0]
+    }
+  },
+  methods: {
+    mouseleave: function () {
+      let self = this
+      self._timer = setTimeout(function () {
+        self.kind = ''
+      }, 150)
+    },
+    enter: function (e) {
+      this.kind = e.target.querySelector('i').className
+    },
+    sover: function () {
+      clearTimeout(this._timer)
+    },
+    sout: function () {
+      this.kind = ''
+    }
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>
