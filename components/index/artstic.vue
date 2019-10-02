a4f372becf6e5ad44f172a22cfd449f2a0aa2dfe
Coray <820563721@qq.com>
2019-10-02 22:45:02 +0800

    mt-app

---
+++ components/index/artstic.vue
@@ -0,0 +1,148 @@
+<template>
+  <section class="m-istyle">
+    <dl @mouseover="over">
+      <dt>有格调</dt>
+      <dd :class="{active:kind==='all'}" :keyword="all" :kind="all">全部</dd>
+      <dd :class="{active:kind==='part'}" :keyword="美食" :kind="part">约会聚餐</dd>
+      <dd :class="{active:kind==='spa'}" :keyword="丽人" :kind="spa">丽人SPA</dd>
+      <dd :class="{active:kind==='movie'}" :keyword="电影" :kind="movie">电影演出</dd>
+      <dd :class="{active:kind==='travel'}" :keyword="旅游" :kind="travel">品质出游</dd>
+    </dl>
+    <ul class="ibody">
+      <li v-for="item in cur" :key="item.title">
+        <el-card :body-style="{padding:'0px'}" shadow="never">
+          <img :src="item.img" alt="" class="image">
+          <ul class="cbody">
+            <li class="title">{{item.title}}</li>
+            <li class="pos"><span>{{item.subtitle}}</span></li>
+            <li class="price">￥<em>{{ item.price }}</em><span>/起</span></li>
+          </ul>
+        </el-card>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      kind: 'all',
+      list: {
+        all: [{
+          title: '恬嬉·法式甜品（人民广场店）',
+          subtitle: '美梦成真1个，约6英寸，仿真',
+          price: Math.ceil(Math.random() * 400),
+          img: '//p1.meituan.net/msmerchant/baee8e8e09c392428917e3f312ecb9b5753122.png',
+          url: '//abc.com'
+        }, {
+          title: '恬嬉·法式甜品（人民广场店）',
+          subtitle: '美梦成真1个，约6英寸，仿真',
+          price: Math.ceil(Math.random() * 400),
+          img: '//p1.meituan.net/msmerchant/baee8e8e09c392428917e3f312ecb9b5753122.png',
+          url: '//abc.com'
+        }, {
+          title: '恬嬉·法式甜品（人民广场店）',
+          subtitle: '美梦成真1个，约6英寸，仿真',
+          price: Math.ceil(Math.random() * 400),
+          img: '//p1.meituan.net/msmerchant/baee8e8e09c392428917e3f312ecb9b5753122.png',
+          url: '//abc.com'
+        }],
+        part: [{
+          title: '恬嬉·法式甜品（人民广场店）',
+          subtitle: '美梦成真1个，约6英寸，仿真',
+          price: Math.ceil(Math.random() * 400),
+          img: '//p1.meituan.net/msmerchant/baee8e8e09c392428917e3f312ecb9b5753122.png',
+          url: '//abc.com'
+        }, {
+          title: '恬嬉·法式甜品（人民广场店）',
+          subtitle: '美梦成真1个，约6英寸，仿真',
+          price: Math.ceil(Math.random() * 400),
+          img: '//p1.meituan.net/msmerchant/baee8e8e09c392428917e3f312ecb9b5753122.png',
+          url: '//abc.com'
+        }, {
+          title: '恬嬉·法式甜品（人民广场店）',
+          subtitle: '美梦成真1个，约6英寸，仿真',
+          price: Math.ceil(Math.random() * 400),
+          img: '//p1.meituan.net/msmerchant/baee8e8e09c392428917e3f312ecb9b5753122.png',
+          url: '//abc.com'
+        }],
+        sap: [{
+          title: '恬嬉·法式甜品（人民广场店）',
+          subtitle: '美梦成真1个，约6英寸，仿真',
+          price: Math.ceil(Math.random() * 400),
+          img: '//p1.meituan.net/msmerchant/baee8e8e09c392428917e3f312ecb9b5753122.png',
+          url: '//abc.com'
+        }, {
+          title: '恬嬉·法式甜品（人民广场店）',
+          subtitle: '美梦成真1个，约6英寸，仿真',
+          price: Math.ceil(Math.random() * 400),
+          img: '//p1.meituan.net/msmerchant/baee8e8e09c392428917e3f312ecb9b5753122.png',
+          url: '//abc.com'
+        }, {
+          title: '恬嬉·法式甜品（人民广场店）',
+          subtitle: '美梦成真1个，约6英寸，仿真',
+          price: Math.ceil(Math.random() * 400),
+          img: '//p1.meituan.net/msmerchant/baee8e8e09c392428917e3f312ecb9b5753122.png',
+          url: '//abc.com'
+        }],
+        movie: [{
+          title: '恬嬉·法式甜品（人民广场店）',
+          subtitle: '美梦成真1个，约6英寸，仿真',
+          price: Math.ceil(Math.random() * 400),
+          img: '//p1.meituan.net/msmerchant/baee8e8e09c392428917e3f312ecb9b5753122.png',
+          url: '//abc.com'
+        }, {
+          title: '恬嬉·法式甜品（人民广场店）',
+          subtitle: '美梦成真1个，约6英寸，仿真',
+          price: Math.ceil(Math.random() * 400),
+          img: '//p1.meituan.net/msmerchant/baee8e8e09c392428917e3f312ecb9b5753122.png',
+          url: '//abc.com'
+        }, {
+          title: '恬嬉·法式甜品（人民广场店）',
+          subtitle: '美梦成真1个，约6英寸，仿真',
+          price: Math.ceil(Math.random() * 400),
+          img: '//p1.meituan.net/msmerchant/baee8e8e09c392428917e3f312ecb9b5753122.png',
+          url: '//abc.com'
+        }],
+        travel: [{
+          title: '恬嬉·法式甜品（人民广场店）',
+          subtitle: '美梦成真1个，约6英寸，仿真',
+          price: Math.ceil(Math.random() * 400),
+          img: '//p1.meituan.net/msmerchant/baee8e8e09c392428917e3f312ecb9b5753122.png',
+          url: '//abc.com'
+        }, {
+          title: '恬嬉·法式甜品（人民广场店）',
+          subtitle: '美梦成真1个，约6英寸，仿真',
+          price: Math.ceil(Math.random() * 400),
+          img: '//p1.meituan.net/msmerchant/baee8e8e09c392428917e3f312ecb9b5753122.png',
+          url: '//abc.com'
+        }, {
+          title: '恬嬉·法式甜品（人民广场店）',
+          subtitle: '美梦成真1个，约6英寸，仿真',
+          price: Math.ceil(Math.random() * 400),
+          img: '//p1.meituan.net/msmerchant/baee8e8e09c392428917e3f312ecb9b5753122.png',
+          url: '//abc.com'
+        }]
+      }
+    }
+  },
+  computed: {
+    cur: function () {
+      return this.list[this.kind]
+    }
+  }
+  // methods: {
+  //   over: function (e) {
+  //     dom = e.target
+  //     let self = this
+  //     this.kind = dom.getAttribute('kind')
+  //   },
+  //
+  // }
+}
+</script>
+
+<style lang="scss">
+  @import "../../assets/css/index/artistic.scss";
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,21 @@
       <el-col :span="5">
         <emenu/>
       </el-col>
-      <el-col :span="19">2</el-col>
+      <el-col :span="19"><life/></el-col>
     </el-row>
-    <el-row :span="24">3</el-row>
+    <el-row :span="24"><artstic/></el-row>
   </div>
 </template>
 
 <script>
 import Emenu from '@/components/index/menu.vue'
+import Life from '@/components/index/life.vue'
+import Artstic from '@/components/index/artstic.vue'
 export default {
   components: {
-    Emenu
+    Emenu,
+    Life,
+    Artstic
   }
 }
 </script>
